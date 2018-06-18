5891c42f5de1b70392547bfe9f6cc86d23618420
Barton Schaefer <shoredata@gmail.com>
2018-06-18 11:25:59 -0700

    Updating queue and slist algorithms.

--- js/slist_exercises.js
+++ js/slist_exercises.js
@@ -48,6 +48,7 @@ SList.prototype.display = function() {
   return this;
 };
 
+
 SList.prototype.add = function(value) {
   console.log("add(" + value+ ")");
   var node = new Node(value),
@@ -248,6 +249,68 @@ SList.prototype.find = function(value) {
 
 }
 
+SList.prototype.zipinorder = function(q) {
+  console.log("zip(" + q+ ")");
+  console.log("Before: ");
+  this.display();
+  q.display();
+  // Main function that inserts nodes of linked list q into p at
+  // alternate positions. Since head of first list never changes
+  // and head of second list/ may change, we need single pointer
+  // for first list and double pointer for second list.
+  var p_current = this.head;
+  var q_current = q.head;
+
+  if (p_current==null) {
+    this.head = q.head;
+  }
+  else{
+    while ((p_current != null) && (q_current != null))  {
+      var p_next = p_current.next;
+      var q_next = q_current.next;
+      if (p_next!= null) {
+        q_current.next = p_next;      // change next pointer of q_curr
+        p_current.next = q_current;   // change next pointer of p_curr
+      }
+      else{
+        p_current.next = q_current;   // change next pointer of p_curr
+      }
+      p_current = p_next;
+      q_current = q_next;
+    }
+  }
+  q.head = null;
+  console.log("After: ");
+  this.display();
+  q.display();
+  return this;
+};
+
+SList.prototype.reverselist = function() {
+  console.log("reverselist()");
+  console.log("Before: ");
+  this.display();
+  var current = this.head;
+  var next = null;
+  var previous = null;
+  // reverse the list
+  while(current != null) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+  // prev is new head of the input list
+  this.head = previous;
+  console.log("After: ");
+  this.display();
+  return this;
+}
+
+
+
+
+
 var console = new Console();
 var list1 = new SList();
 list1.add(1)
@@ -280,10 +343,50 @@ list1.front()
 list1.end()
 
 
+var list1 = new SList();
+var list2 = new SList();
 
-// OUTPUT::
-//
-// 11:04:00 bart ~/projects/cd/algorithms/slist
+// list1.add(11);
+// list1.add(12);
+// list1.add(13);
+list1.add(14);
+list2.add(21);
+list2.add(22);
+list2.add(23);
+list2.add(24);
+list1.zipinorder(list2);
+// list1.reverselist();
+
+
+var list3 = new SList()
+var list4 = new SList();
+
+list3.add(31);
+list3.add(32);
+list3.add(33);
+list3.add(34);
+list4.add(41);
+list3.zipinorder(list4);
+
+var list5 = new SList()
+var list6 = new SList();
+
+list5.add(51);
+list5.add(52);
+list6.add(61);
+list6.add(62);
+list5.zipinorder(list6);
+
+list1.reverselist();
+list3.reverselist();
+list5.reverselist();
+
+
+
+
+// // OUTPUT::
+// //
+// 11:19:53 bart ~/projects/cd/algorithms/js (master)
 // $ js slist_exercises.js
 // --> add(1)
 // --> 1
@@ -340,3 +443,67 @@ list1.end()
 // --> average = 2.6666666666666665
 // --> front = 1
 // --> end = 4
+// --> add(14)
+// --> 14
+// --> add(21)
+// --> 21
+// --> add(22)
+// --> 21,22
+// --> add(23)
+// --> 21,22,23
+// --> add(24)
+// --> 21,22,23,24
+// --> zip([object Object])
+// --> Before:
+// --> 14
+// --> 21,22,23,24
+// --> After:
+// --> 14,21,22,23,24
+// -->
+// --> add(31)
+// --> 31
+// --> add(32)
+// --> 31,32
+// --> add(33)
+// --> 31,32,33
+// --> add(34)
+// --> 31,32,33,34
+// --> add(41)
+// --> 41
+// --> zip([object Object])
+// --> Before:
+// --> 31,32,33,34
+// --> 41
+// --> After:
+// --> 31,41,32,33,34
+// -->
+// --> add(51)
+// --> 51
+// --> add(52)
+// --> 51,52
+// --> add(61)
+// --> 61
+// --> add(62)
+// --> 61,62
+// --> zip([object Object])
+// --> Before:
+// --> 51,52
+// --> 61,62
+// --> After:
+// --> 51,61,52,62
+// -->
+// --> reverselist()
+// --> Before:
+// --> 14,21,22,23,24
+// --> After:
+// --> 24,23,22,21,14
+// --> reverselist()
+// --> Before:
+// --> 31,41,32,33,34
+// --> After:
+// --> 34,33,32,41,31
+// --> reverselist()
+// --> Before:
+// --> 51,61,52,62
+// --> After:
+// --> 62,52,61,51
---
+++ slist/queue_exercises.js
@@ -0,0 +1,84 @@
+function Node(value) {
+  this.value = value;
+  this.next = null;
+};
+function QueueList(){
+  this.head = null;
+  this.tail = null;
+};
+Console = function () {
+  this.log = function(msg){ debug(msg) };
+};
+
+
+QueueList.prototype.display = function() {
+  var mylist = []
+  var current = this.head;
+  while (current) {
+    mylist.push(current.value);
+    current = current.next;
+  }
+  console.log(mylist);
+  return this;
+};
+
+QueueList.prototype.addend = function(value){
+  console.log("addend(" + value + ")");
+  this.display();
+  var node = new Node(value);
+  if (this.head==null) {
+    this.head = node;
+    this.tail=node
+  }
+  else {
+    this.tail.next = node;
+    this.tail = node;
+  }
+  this.display();
+  return this;
+};
+
+QueueList.prototype.removefront = function() {
+  console.log("removefront()");
+  this.display();
+  if (this.head==this.tail){
+    //catches empty list and list of 1
+    this.head=null;
+    this.tail=null;
+  }
+  else{
+    var temp = this.head;
+    this.head = this.head.next;
+    temp.next = null; // remove ref for garbage collection
+  }
+  this.display();
+  return this;
+};
+
+var console = new Console();
+q1 = new QueueList();
+q1.addend(1);
+q1.removefront();
+q1.addend(10);
+q1.addend(11);
+q1.removefront();
+
+
+// OUTPUT::
+// 11:09:35 bart ~/projects/cd/algorithms/slist
+// $ js queue_exercises.js
+// --> addend(1)
+// -->
+// --> 1
+// --> removefront()
+// --> 1
+// -->
+// --> addend(10)
+// -->
+// --> 10
+// --> addend(11)
+// --> 10
+// --> 10,11
+// --> removefront()
+// --> 10,11
+// --> 11
