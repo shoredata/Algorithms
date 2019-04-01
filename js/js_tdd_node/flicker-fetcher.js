cc6889ebba5f10e2e6486cab0a0e68f716b2aa74
shoredata <31630233+shoredata@users.noreply.github.com>
2019-04-01 10:42:26 -0700

    Adding network request functions and tests.

--- js/js_tdd_node/flicker-fetcher.js
+++ js/js_tdd_node/flicker-fetcher.js
@@ -16,7 +16,39 @@ FlickrFetcher = {
             title: photoObj.title,
             url:   FlickrFetcher.photoObjToURL(photoObj)
         };
-    }
+    },
+    
+    // fetchFlickrData: function(apiKey, fetch) {
+    //     var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
+    //             + apiKey + '&text=pugs&format=json&nojsoncallback=1'
+    //     return fetch(url).then(function(data) {
+    //         return data;
+    //     });
+    // }
+
+    // Running the tests again, everything still passes. But I would also like to refactor my test 
+    //  code. Mocha actually provides two ways to handle asynchronous code. The first is the done() 
+    //  function as we saw before. The second is specifically for Promises. If you return a Promise 
+    //  from your test, Mocha will automatically wait for it to either resolve or reject:
+
+    fetchFlickrData: function(apiKey, fetch) {
+        var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
+                + apiKey + '&text=pugs&format=json&nojsoncallback=1'
+        return fetch(url);
+    },
+    
+    
+    // There is one final thing to test before I can declare the FlickrFetcher module done: 
+    //  Do the pieces fit together OK? Can I make a network call, get back the results, and 
+    //  transform them into the format I want? It would be most convenient if I could do all 
+    //  this with one function.
+
+    fetchPhotos: function(apiKey, fetch) {
+        return FlickrFetcher.fetchFlickrData(apiKey, fetch).then(function(data) {
+            return data.photos.photo.map(FlickrFetcher.transformPhotoObj);
+        });
+    }    
+
 };
 
 module.exports = FlickrFetcher;
--- js/js_tdd_node/test/flicker-fetcher-spec.js
+++ js/js_tdd_node/test/flicker-fetcher-spec.js
@@ -27,6 +27,7 @@ var expect = require('chai').expect;
 
 // TDD FULL CYCLE: Red, Green, Refactor
 
+// at promtp run "nodemon --exec mocha" in project folder to run tests
 
 var FlickrFetcher;
 describe('FlickrFetcher', function() {
@@ -131,5 +132,124 @@ describe('#transformPhotoObj()', function() {
         actual = FlickrFetcher.transformPhotoObj(input);
         expect(actual).to.eql(expected);
 
+    });
+});
+
+// In the application code, I would pass $.getJSON as the fetch parameter (more on that 
+//  later). In my test though, I can pass a fake getJSON() method that always returns a 
+//  promise for the same data. Then I can check that my function returns exactly what I 
+//  expect, without making a network call.
+
+// The other thing that is tricky about network calls with JavaScript is that they are 
+//  asyncrhonous. This means that we need some way of telling our test runner (Mocha) 
+//  to wait until all the tests finish. Mocha provides a parameter to the it() callback 
+//  called done that allows us to tell Mocha when the test is complete.
+
+//stubbed:
+// Stubs do take a little bit of effort to use well though. For instance, using a stub 
+//  meant adding an extra parameter to the fetchFlickrData() function. However, if you 
+//  are using a slightly functional-flavoured style of programming, then you will be 
+//  thinking about things like side effects and pure functions anyway. I would also 
+//  argue that making your code testable (whether that’s using stubs or not) is 
+//  usually worth the effort.
+
+describe('#fetchFlickrData()', function() {
+    it(
+        'should take an API key and fetcher function argument and return a promise for JSON data.',
+        function() {
+            var apiKey      = 'does not matter much what this is right now',
+                fakeData    = {
+                    'photos': {
+                        'page':    1,
+                        'pages':   2872,
+                        'perpage': 100,
+                        'total':   '287170',
+                        'photo':   [{
+                            'id':       '24770505034',
+                            'owner':    '97248275@N03',
+                            'secret':   '31a9986429',
+                            'server':   '1577',
+                            'farm':     2,
+                            'title':    '20160229090898',
+                            'ispublic': 1,
+                            'isfriend': 0,
+                            'isfamily': 0
+                        }, {
+                            'id':       '24770504484',
+                            'owner':    '97248275@N03',
+                            'secret':   '69dd90d5dd',
+                            'server':   '1451',
+                            'farm':     2,
+                            'title':    '20160229090903',
+                            'ispublic': 1,
+                            'isfriend': 0,
+                            'isfamily': 0
+                        }]
+                    }
+                },
+                fakeFetcher = function(url) {
+                    var expectedURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
+                                + apiKey + '&text=pugs&format=json&nojsoncallback=1'
+                    expect(url).to.equal(expectedURL)
+                    return Promise.resolve(fakeData);
+                };
+            return FlickrFetcher.fetchFlickrData(apiKey, fakeFetcher).then(function(actual) {
+                expect(actual).to.eql(fakeData);
+            }
+        );
+
+    });
+});
+
+
+describe('#fetchPhotos()', function() {
+    it('should take an API key and fetcher function, and return a promise for transformed photos', function() {
+        var apiKey   = 'does not matter what this is right now',
+            expected = [{
+                title: 'Dog goes to desperate measure to avoid walking on a leash',
+                url:   'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg'
+            }, {
+                title: 'the other cate',
+                url:   'https://farm2.staticflickr.com/1514/24765033584_3c190c104e_b.jpg'
+            }],
+            fakeData = {
+                'photos': {
+                    'page':    1,
+                    'pages':   2872,
+                    'perpage': 100,
+                    'total':   '287170',
+                    'photo':   [{
+                        id:       '25373736106',
+                        owner:    '99117316@N03',
+                        secret:   '146731fcb7',
+                        server:   '1669',
+                        farm:     2,
+                        title:    'Dog goes to desperate measure to avoid walking on a leash',
+                        ispublic: 1,
+                        isfriend: 0,
+                        isfamily: 0
+                    }, {
+                        id:       '24765033584',
+                        owner:    '27294864@N02',
+                        secret:   '3c190c104e',
+                        server:   '1514',
+                        farm:     2,
+                        title:    'the other cate',
+                        ispublic: 1,
+                        isfriend: 0,
+                        isfamily: 0
+                    }]
+                }
+            },
+            fakeFetcher = function(url) {
+                var expectedURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
+                            + apiKey + '&text=pugs&format=json&nojsoncallback=1'
+                expect(url).to.equal(expectedURL)
+                return Promise.resolve(fakeData);
+            };
+
+        return FlickrFetcher.fetchPhotos(apiKey, fakeFetcher).then(function(actual) {
+            expect(actual).to.eql(expected);
+        });
     });
 });
