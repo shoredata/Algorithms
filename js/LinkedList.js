e59c6760fdd63b89fd32796a4e59d817654d9351
shoredata <31630233+shoredata@users.noreply.github.com>
2019-04-19 16:48:22 -0700

    Cleaning up tests for Jest.

--- js/LinkedList.js
+++ js/LinkedList.js
@@ -3,43 +3,29 @@ import Comparator from './Comparator';
 
 export default class LinkedList {
   constructor(comparatorFunction) {
-    /** @var LinkedListNode */
     this.head = null;
-
-    /** @var LinkedListNode */
     this.tail = null;
-
     this.compare = new Comparator(comparatorFunction);
   }
 
   prepend(value) {
-    // Make new node to be a head.
     const newNode = new LinkedListNode(value, this.head);
     this.head = newNode;
-
-    // If there is no tail yet let's make new node a tail.
     if (!this.tail) {
       this.tail = newNode;
     }
-
     return this;
   }
 
   append(value) {
     const newNode = new LinkedListNode(value);
-
-    // If there is no head yet let's make new node a head.
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
-
       return this;
     }
-
-    // Attach new node to the end of linked list.
     this.tail.next = newNode;
     this.tail = newNode;
-
     return this;
   }
 
@@ -50,8 +36,6 @@ export default class LinkedList {
 
     let deletedNode = null;
 
-    // If the head must be deleted then make next node that is differ
-    // from the head to be a new head.
     while (this.head && this.compare.equal(this.head.value, value)) {
       deletedNode = this.head;
       this.head = this.head.next;
@@ -60,7 +44,6 @@ export default class LinkedList {
     let currentNode = this.head;
 
     if (currentNode !== null) {
-      // If next node must be deleted then make next node to be a next next one.
       while (currentNode.next) {
         if (this.compare.equal(currentNode.next.value, value)) {
           deletedNode = currentNode.next;
@@ -71,7 +54,6 @@ export default class LinkedList {
       }
     }
 
-    // Check if tail must be deleted.
     if (this.compare.equal(this.tail.value, value)) {
       this.tail = currentNode;
     }
@@ -87,12 +69,10 @@ export default class LinkedList {
     let currentNode = this.head;
 
     while (currentNode) {
-      // If callback is specified then try to find node by callback.
       if (callback && callback(currentNode.value)) {
         return currentNode;
       }
 
-      // If value is specified then try to compare by value..
       if (value !== undefined && this.compare.equal(currentNode.value, value)) {
         return currentNode;
       }
@@ -107,16 +87,11 @@ export default class LinkedList {
     const deletedTail = this.tail;
 
     if (this.head === this.tail) {
-      // There is only one node in linked list.
       this.head = null;
       this.tail = null;
-
       return deletedTail;
     }
 
-    // If there are many nodes in linked list...
-
-    // Rewind to the last node and delete "next" link for the node before the last one.
     let currentNode = this.head;
     while (currentNode.next) {
       if (!currentNode.next.next) {
@@ -176,18 +151,12 @@ export default class LinkedList {
     let nextNode = null;
 
     while (currNode) {
-      // Store next node.
       nextNode = currNode.next;
-
-      // Change next node of the current node so it would link to previous node.
       currNode.next = prevNode;
-
-      // Move prevNode and currNode nodes one step forward.
       prevNode = currNode;
       currNode = nextNode;
     }
 
-    // Reset head and tail.
     this.tail = this.head;
     this.head = prevNode;
 
