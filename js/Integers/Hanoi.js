283500a688818a2dd5fb1573d828f85675483dbf
Barton Schaefer <shoredata@gmail.com>
2019-04-30 07:35:55 -0700

    Updating js Hanoi with unit tests.

--- js/Integers/Hanoi.js
+++ js/Integers/Hanoi.js
@@ -1,4 +1,4 @@
-function hanoi(disc, src, dst, aux=[]) {
+export function hanoi(disc, src, dst, aux=[]) {
   if (disc === 0) {
     // var disk = src.pop();
     // dst.push(disk);
@@ -9,7 +9,7 @@ function hanoi(disc, src, dst, aux=[]) {
     hanoi(disc-1, aux, dst, src);
   }
 }
-var ar1 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], ar2=[], ar3=[];
-console.log(ar1, ar2, ar3);
-hanoi(10, ar1, ar2, ar3);
-console.log(ar1, ar2, ar3);
+// var ar1 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], ar2=[], ar3=[];
+// console.log(ar1, ar2, ar3);
+// hanoi(10, ar1, ar2, ar3);
+// console.log(ar1, ar2, ar3);
---
+++ js/test/Hanoi.test.js
@@ -0,0 +1,16 @@
+import * as han from '../Integers/Hanoi';
+
+describe('hanoi()', () => {
+
+  it('hanoi of 10 discs on 3 towers', () => {
+    var ar1 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], ar2=[], ar3=[];
+    // console.log(ar1, ar2, ar3);
+    han.hanoi(10, ar1, ar2, ar3);
+    // console.log(ar1, ar2, ar3);
+    expect(ar1).toEqual([]);
+    expect(ar2).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+    expect(ar3).toEqual([]);
+  });
+
+
+});
